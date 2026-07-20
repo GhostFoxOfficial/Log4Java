a0a9896abe0ba8c3b1b08b48ea4b59d2645c2718
Sprinklekit <sprinklekit.original@gmail.com>
2026-07-20 10:29:13 -0700

    Update Tools.java
    Integrating KivyAI

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -92,7 +92,7 @@ public final class Tools {
 
     public static final Gson GLOBAL_GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    public static final String URL_HOME = "https://mojolauncher.ru";
+    public static final String URL_HOME = "http://localhost:7860";
     public static String NATIVE_LIB_DIR;
     public static String DIR_DATA; //Initialized later to get context
     public static File DIR_CACHE;
