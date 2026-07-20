c2ac4248399c25c8d7bc3fbc6a0cb18fef7242ae
Sprinklekit <sprinklekit.original@gmail.com>
2026-07-20 12:42:36 -0700

    Create KivyChatActivity.java

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyChatActivity.java
@@ -0,0 +1,44 @@
+package net.kdt.pojavlaunch;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import t.artdeell.mojo.utils.Tools;
+
+public class KivyChatActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kivy_chat);
+
+        mWebView = findViewById(R.id.kivy_webview);
+
+        
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+
+        
+        mWebView.setWebViewClient(new WebViewClient());
+
+        
+        mWebView.loadUrl(Tools.URL_HOME);
+    }
+
+    
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
