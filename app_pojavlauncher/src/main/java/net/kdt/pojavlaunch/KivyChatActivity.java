64417c6fd38ca6e5cb43c15d2bd3ecbf34ade1d2
Sprinklekit <sprinklekit.original@gmail.com>
2026-07-20 13:01:03 -0700

    Update KivyChatActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyChatActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyChatActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Resources;
 
 
-import t.artdeell.mojo.utils.Tools;
+import t.artdeell.mojo.Tools;
 
 public class KivyChatActivity extends AppCompatActivity {
 
