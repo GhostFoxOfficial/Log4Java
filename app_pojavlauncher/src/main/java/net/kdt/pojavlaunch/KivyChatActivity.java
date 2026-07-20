cc873775cfc7fd2e8a8ff28f0dab2a1af6c669cb
Sprinklekit <sprinklekit.original@gmail.com>
2026-07-20 15:12:37 -0700

    Update KivyChatActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyChatActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyChatActivity.java
@@ -47,7 +47,7 @@ public class KivyChatActivity extends AppCompatActivity {
 
         mWebView.setWebViewClient(new WebViewClient());
 
-        mWebView.addJavascriptInterface(new KivyAgentBridge(this),
+        mWebView.addJavascriptInterface(new KivyAgentBridge(this);
         
         mWebView.loadUrl(Tools.URL_HOME);
     }
