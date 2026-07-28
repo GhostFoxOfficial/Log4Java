deda270d875a0e0e7338421172323e4d98401132
Mutant Bloodcell <mutantbloodcell@gmail.com>
2026-07-28 20:51:54 +1200

    Fix RAM allocation notice message formatting

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyAgentBridge.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/KivyAgentBridge.java
@@ -51,10 +51,10 @@ public class KivyAgentBridge {
 
                 case "SET_RAM":
                     int ramValue = Integer.parseInt(value);
-                    net.kdt.pojavlaunch.prefs.LauncherPreferences.PREF_RAM_ALLOCATION.edit()
+                    net.kdt.pojavlaunch.prefs.LauncherPreferences.DEFAULT_PREF.edit()
                         .putInt("allocation", ramValue)
                         .apply();
-                    showNotice("Kivy: Allocated RAM set to " + ramValue + "MB");
+                    showNotice("Kivy: Allocated RAM set to " + ramValue + " MB");
                     break;
 
                 case "SET_RENDERER":
